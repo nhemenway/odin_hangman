f144ddb73159f6f05209426d18003826d1321da2
Nicholas Hemenway <n@nhem.co>
2017-02-27 08:49:49 -0800

    working saving loading

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,11 @@
+require "json"
+
 class Game
-  def initialize
+  def initialize(skip_select = false)
     @secret = make_secret
-    @guess_count = 0
     @guesses = []
-    play
+    play if skip_select
+    select_game
   end
 
   def make_secret
@@ -21,17 +23,23 @@ class Game
   def play
     until game_over?
       display_status
-      puts "Make your guess. You have #{12 - @guess_count} remaining."
-      while true
-        guess = gets.chomp[0].upcase
-        if legal_guess?(guess) then break end
-        puts "Illegal guess. Try again."
-      end
-      @guesses << guess
-      @guess_count += 1
+      puts "Make your guess. You have #{12 - @guesses.length} remaining."
+      guess = get_guess
       check(guess)
     end
     end_game
+  end
+
+  def get_guess
+    while true
+      guess = gets.chomp.upcase
+      break if legal_guess?(guess) || guess == "SAVE"
+      puts "Illegal guess. Try again."
+    end
+
+    save if guess == "SAVE"
+    @guesses << guess
+    return guess
   end
 
   def check(guess)
@@ -46,12 +54,49 @@ class Game
     !@guesses.include?(guess) && ("A".."Z").include?(guess)
   end
 
-  def display_status
-    result = []
-    @secret.each do |l|
-      @guesses.include?(l) ? result << l : result << "_"
+  def save
+    game_status = {:time => Time.now, :secret => @secret, :guesses => @guesses}
+
+    File.open("saves.json", "a") do |f|
+      f.puts(game_status.to_json)
     end
-    puts result.join(' ') + ' || ' + @guesses.join
+    puts "You saved the game!"
+    select_game
+  end
+
+  def show_saves
+    File.open('saves.json').readlines.each_with_index do |l, i|
+      json = JSON.parse(l)
+      date = json["time"]
+      puts "Load (#{i}) from #{date}"
+      display_status(json["secret"], json["guesses"])
+    end
+    select_game
+  end
+
+  def select_game
+    puts "Would you like to (c)ontinue, start a (n)ew game or (l)oad a save (number)?"
+    response = gets.chomp.upcase
+    Game.new(true) if response == "N" && !@guesses.empty?
+    play if response == "C" || (response == "N" && @guesses.empty?)
+    show_saves if response == "L" 
+    load_save(response.to_i) =~ /d/
+    play
+  end
+
+  def load_save(n)
+    f = File.open('saves.json').readlines[n]
+    json = JSON.parse(f)
+    @secret = json["secret"]
+    @guesses = json["guesses"]
+  end
+
+  def display_status(secret = @secret, guesses = @guesses)
+    result = []
+    secret.each do |l|
+      guesses.include?(l) ? result << l : result << "_"
+    end
+    puts result.join(' ') + ' || ' + guesses.join
   end
 
   def game_over?
@@ -59,7 +104,7 @@ class Game
   end
 
   def loss?
-    @guess_count == 12
+    @guesses.length == 12
   end
 
   def won?
@@ -81,6 +126,17 @@ class Game
     puts "Let's play again."
     Game.new
   end
+
+  # def to_json(*a)
+  #   {
+  #     "json_class"   => self.class.name,
+  #     "data"         => {"secret" => @secret, "guesses" => @guesses}
+  #   }.to_json(*a)
+  # end
+
+  # def self.json_create(o)
+  #   new(o["data"]["string"], o["data"]["number"])
+  # end
 
 end
 
